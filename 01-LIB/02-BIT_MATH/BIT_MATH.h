958cd2029a646f19434b1ce911013caf46aaa862
Mohamed Ayman <134950534+mohamedaymankills@users.noreply.github.com>
2024-06-27 12:14:06 +0300

    Update BIT_MATH.h

--- 01-LIB/02-BIT_MATH/BIT_MATH.h
+++ 01-LIB/02-BIT_MATH/BIT_MATH.h
@@ -1,19 +1,53 @@
-//               بسم الله الرحمن الرحيم 
+/****************************************************************/
+/******* Author    : Mohamed Ayman              *****************/
+/******* Date      : 20 jun 2023                *****************/
+/******* Version   : 0.0.1                      *****************/
+/******* File Name : BIT_MATH.h                 *****************/
+/****************************************************************/
+
+#ifndef BIT_MATH_H_
+#define BIT_MATH_H_
+
+/**
+ * @brief Set a specific bit in a register.
+ *
+ * This macro sets the specified bit at the given position in a register to 1.
+ *
+ * @param[in,out] REG The register in which to set the bit.
+ * @param[in] BIT_NUMBER The position of the bit to set (0 to 31).
+ */
+#define SET_BIT(REG, BIT_NUMBER)  REG |= (1 << BIT_NUMBER)
+
+/**
+ * @brief Clear a specific bit in a register.
+ *
+ * This macro clears (sets to 0) the specified bit at the given position in a register.
+ *
+ * @param[in,out] REG The register in which to clear the bit.
+ * @param[in] BIT_NUMBER The position of the bit to clear (0 to 31).
+ */
+#define CLR_BIT(REG, BIT_NUMBER)  REG &= ~(1 << BIT_NUMBER)
+
+/**
+ * @brief Toggle a specific bit in a register.
+ *
+ * This macro toggles (inverts) the state of the specified bit at the given position in a register.
+ *
+ * @param[in,out] REG The register in which to toggle the bit.
+ * @param[in] BIT_NUMBER The position of the bit to toggle (0 to 31).
+ */
+#define TOG_BIT(REG, BIT_NUMBER)  REG ^= (1 << BIT_NUMBER)
+
+/**
+ * @brief Get the value of a specific bit in a register.
+ *
+ * This macro retrieves the value (0 or 1) of the specified bit at the given position in a register.
+ *
+ * @param[in] REG The register from which to get the bit value.
+ * @param[in] BIT_NUMBER The position of the bit to get (0 to 31).
+ * @return The value of the specified bit (0 or 1).
+ */
+#define GET_BIT(REG, BIT_NUMBER)  ((REG & (1 << BIT_NUMBER)) >> BIT_NUMBER)
 
 
-
-
-/*    Author    : 3mo MOHAMED AYMAN ABDELAZIZ                          */
-
-
-
-
-#ifndef BIT_MATH_H
-#define BIT_MATH_H
-
-#define SET_BIT(REG,BIT_POS)       (REG|=(1<<BIT_POS))    
-#define CLR_BIT(REG,BIT_POS)       (REG&=~(1<<BIT_POS))    
-#define TOG_BIT(REG,BIT_POS)       (REG^=(1<<BIT_POS))    
-#define GET_BIT(REG,BIT_POS)       ((REG>>BIT_POS) &1) 
-
-#endif
+#endif /* BIT_MATH_H_ */
