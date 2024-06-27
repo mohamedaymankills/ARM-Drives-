b45e777c878ce7bf5a8a265d7779441143f31031
Mohamed Ayman <134950534+mohamedaymankills@users.noreply.github.com>
2024-06-27 12:13:07 +0300

    Update STD_TYPES.h

--- 01-LIB/01-STD_TYPES/STD_TYPES.h
+++ 01-LIB/01-STD_TYPES/STD_TYPES.h
@@ -1,22 +1,45 @@
-//               بسم الله الرحمن الرحيم 
+/****************************************************************/
+/******* Author    : Mohamed Ayman              *****************/
+/******* Date      : 20 jun 2023                *****************/
+/******* Version   : 0.0.1                      *****************/
+/******* File Name : RCC_programe.c             *****************/
+/****************************************************************/
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+
+/**< Include standard integer types for fixed-size definitions */ 
+#include <stdint.h> 
+
+typedef uint8_t  u8;           /**< 8-bit unsigned integer type */
+typedef uint16_t u16;          /**< 16-bit unsigned integer type */
+typedef uint32_t u32;          /**< 32-bit unsigned integer type */
+typedef uint64_t u64;          /**< 64-bit unsigned integer type */
+
+typedef int8_t  s8;            /**< 8-bit signed integer type */
+typedef int16_t s16;           /**< 16-bit signed integer type */
+typedef int32_t s32;           /**< 32-bit signed integer type */
+typedef int64_t s64;           /**< 64-bit signed integer type */
+
+typedef float  f32;            /**<  Single-precision floating-point type */
+typedef double f64;            /**<  Double-precision floating-point type */
+typedef long double f96;       /**<  Extended-precision floating-point type */
+
+typedef u8 Std_ReturnType;     /**< Standard return type for functions */
+typedef s8 INVALID_VALUE;      /**< Marker for invalid or uninitialized data */
 
 
 
-
-/*    Author    : 3mo MOHAMED AYMAN ABDELAZIZ                          */
-
+#define NULL ((void *)0x0)     /**< Null pointer definition */
 
 
 
+#define E_OK ((Std_ReturnType)1)                    /**< Function execution successful */
+#define E_NOT_OK ((Std_ReturnType)0)                /**< Function execution failed */
 
-#ifndef STD_TYPES_H
-#define STD_TYPES_H
+#define E_INVALID_PARAMETER  ((INVALID_VALUE)-1)    /**< Invalid parameter marker */
 
-typedef unsigned char u8;
-typedef unsigned short u16;
-typedef unsigned int u32;
-typedef signed char s8;
-typedef signed short s6;
-typedef signed int s32;
 
-#endif
+
+#endif /* STD_TYPES_H_ */
+
+
